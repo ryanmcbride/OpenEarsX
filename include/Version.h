feb8bd4b96ee01a009f013a749327678b5361ac6
Admin <administrator@Ryans-iMac-2.local>
2014-08-02 11:58:30 -0600

    initial project setup

---
+++ include/Version.h
@@ -0,0 +1,13 @@
+#if 0
+//
+//  Version.h
+//  OpenEars
+//
+//  Created by Halle on 9/24/12.
+//  Copyright (c) 2012 Politepix. All rights reserved.
+//
+
+#define kCurrentVersionTokenized @"{{{{1.71}}}}"
+#define kCurrentVersion [[kCurrentVersionTokenized stringByReplacingOccurrencesOfString:@"{" withString:@""] stringByReplacingOccurrencesOfString:@"}" withString:@""]
+
+#endif
